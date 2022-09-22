361797714ee112842ae6d13ffe3ee14902990449
Ali Can Yelekci <acyelekci@gmail.com>
2022-09-23 00:40:43 +0300

    added grid generation button

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
+let gridColumns;
+let gridSize;
+const btn = document.querySelector("button");
 const grid = document.createElement("div");
 grid.className = "grid-container";
 grid.id = "hello";
 document.body.appendChild(grid);
 
-let gridSize = 4;
-let gridColumns = Array(gridSize).fill("1fr").join(" ");
-document.querySelector(".grid-container").style["grid-template-columns"] = gridColumns;
+btn.addEventListener("click", generateGrid);
 
+function generateGrid() {
+    gridSize = document.querySelector("input").value;
+    gridColumns = Array(parseInt(gridSize)).fill("1fr").join(" ");
+    document.querySelector(".grid-container").style["grid-template-columns"] = gridColumns;
 
-for (let i = 1; i <= gridSize ** 2; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.className = "grid-item";
-    newDiv.id = `item${i}`;
-    grid.appendChild(newDiv);
+    for (let i = 1; i <= gridSize ** 2; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.className = "grid-item";
+        newDiv.id = `item${i}`;
+        grid.appendChild(newDiv);
+    }
+    
+    const blocks = document.querySelectorAll('.grid-item');
+    blocks.forEach((block) => {
+        block.addEventListener('mouseover', () => changeColor(block.id, "black"));
+    });
 }
-
-const blocks = document.querySelectorAll('.grid-item');
-blocks.forEach((block) => {
-    block.addEventListener('mouseover', () => changeColor(block.id, "black"));
-});
 
 function changeColor(id,color) {
     return document.querySelector(`#${id}`).style.backgroundColor = color;
